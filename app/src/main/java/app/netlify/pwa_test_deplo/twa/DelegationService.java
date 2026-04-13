eb0db461a0068088c02700020b27e8e1d157914a
arseni2 <arc-37@mail.ru>
2026-04-13 12:36:42 +0300

    add

---
+++ app/src/main/java/app/netlify/pwa_test_deplo/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.netlify.pwa_test_deplo.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
